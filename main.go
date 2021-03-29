e7d445ef01e9f030e35805ec93e5752c5edc6ce2
Dominik Froehlich <Dominik.Frolic@gmail.com>
2021-03-29 14:22:04 +0200

    Add connection rate option

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ import (
 var address = flag.String("address", "", "the address to connect to e.g. www.myhost.com:443")
 var useTLS = flag.Bool("tls", true, "whether to use TLS to connect or not")
 var maxConn = flag.Int("connections", 100, "maximum number of concurrent connections to open")
+var connRate = flag.Int("rate", 0, "Connection rate in connections / second. Zero means no rate limit.")
 var wait = flag.Duration("wait", 5*time.Minute, "time to wait before tearing down connections again")
+
+var timePerConn time.Duration
 
 func main() {
 
@@ -21,6 +24,10 @@ func main() {
 	if *address == "" {
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if *connRate > 0 {
+		timePerConn = time.Second / time.Duration(*connRate)
 	}
 
 	if *useTLS {
@@ -51,6 +58,9 @@ func connectTCP(addr string, maxConn int, wait time.Duration) {
 			fmt.Printf("%s -> %s (%d, took %s)\n", conn.LocalAddr().String(), conn.RemoteAddr().String(), i, dur)
 			connections = append(connections, conn)
 		}
+		if dur < timePerConn {
+			time.Sleep(timePerConn - dur)
+		}
 	}
 
 	fmt.Printf("\nWaiting for %s...\n\n", wait)
@@ -74,10 +84,14 @@ func connectTLS(addr string, maxConn int, wait time.Duration) {
 		KeepAlive: 10 * time.Second,
 	}
 
+	c := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
 	fmt.Println("Opening TLS connections...")
 	for i := 1; i <= maxConn; i++ {
 		tStart := time.Now()
-		conn, err := tls.DialWithDialer(d, "tcp", addr, nil)
+		conn, err := tls.DialWithDialer(d, "tcp", addr, c)
 		tEnd := time.Now()
 		dur := tEnd.Sub(tStart)
 		if err != nil {
@@ -86,6 +100,9 @@ func connectTLS(addr string, maxConn int, wait time.Duration) {
 		if conn != nil {
 			fmt.Printf("%s -> %s (%d, took %s)\n", conn.LocalAddr().String(), conn.RemoteAddr().String(), i, dur)
 			connections = append(connections, conn)
+		}
+		if dur < timePerConn {
+			time.Sleep(timePerConn - dur)
 		}
 	}
 
